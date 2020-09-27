4c8d3ff920d16498555f70641bf4de11c0e991d8
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2020-09-28 00:14:33 +0900

    added more ML algorithm

--- ml.py
+++ ml.py
@@ -1,19 +1,44 @@
 from sqlalchemy.orm import aliased
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import preprocessing
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 
 from models import Candle 
 from models import Order 
 from base import Session
+from utility import createNumericCandleDict
+from utility import createCategoricCandleList
+from encoder import MultiColumnLabelEncoder
+
 session = Session()
 
-C0_VARIABLES = ["c0_Open", "c0_High", "c0_Low", "c0_Close", "c0_Volume", "c0_Quote_Asset_Volume", "c0_Number_Of_Trades", "c0_Taker_Buy_Base_Asset_Volume", "c0_Taker_Buy_Quote_Asset_Volume" ]
-C1_VARIABLES = ["c1_Open", "c1_High", "c1_Low", "c1_Close", "c1_Volume", "c1_Quote_Asset_Volume", "c1_Number_Of_Trades", "c1_Taker_Buy_Base_Asset_Volume", "c1_Taker_Buy_Quote_Asset_Volume" ]
-C2_VARIABLES = ["c2_Open", "c2_High", "c2_Low", "c2_Close", "c2_Volume", "c2_Quote_Asset_Volume", "c2_Number_Of_Trades", "c2_Taker_Buy_Base_Asset_Volume", "c2_Taker_Buy_Quote_Asset_Volume" ]
-C3_VARIABLES = ["c3_Open", "c3_High", "c3_Low", "c3_Close", "c3_Volume", "c3_Quote_Asset_Volume", "c3_Number_Of_Trades", "c3_Taker_Buy_Base_Asset_Volume", "c3_Taker_Buy_Quote_Asset_Volume" ]
-C4_VARIABLES = ["c4_Open", "c4_High", "c4_Low", "c4_Close", "c4_Volume", "c4_Quote_Asset_Volume", "c4_Number_Of_Trades", "c4_Taker_Buy_Base_Asset_Volume", "c4_Taker_Buy_Quote_Asset_Volume" ]
+C0_NUM_VARIABLES = ["c0_Open", "c0_High", "c0_Low", "c0_Close", "c0_Volume", "c0_Quote_Asset_Volume", "c0_Number_Of_Trades", "c0_Taker_Buy_Base_Asset_Volume", "c0_Taker_Buy_Quote_Asset_Volume" ]
+C1_NUM_VARIABLES = ["c1_Open", "c1_High", "c1_Low", "c1_Close", "c1_Volume", "c1_Quote_Asset_Volume", "c1_Number_Of_Trades", "c1_Taker_Buy_Base_Asset_Volume", "c1_Taker_Buy_Quote_Asset_Volume" ]
+C2_NUM_VARIABLES = ["c2_Open", "c2_High", "c2_Low", "c2_Close", "c2_Volume", "c2_Quote_Asset_Volume", "c2_Number_Of_Trades", "c2_Taker_Buy_Base_Asset_Volume", "c2_Taker_Buy_Quote_Asset_Volume" ]
+C3_NUM_VARIABLES = ["c3_Open", "c3_High", "c3_Low", "c3_Close", "c3_Volume", "c3_Quote_Asset_Volume", "c3_Number_Of_Trades", "c3_Taker_Buy_Base_Asset_Volume", "c3_Taker_Buy_Quote_Asset_Volume" ]
+C4_NUM_VARIABLES = ["c4_Open", "c4_High", "c4_Low", "c4_Close", "c4_Volume", "c4_Quote_Asset_Volume", "c4_Number_Of_Trades", "c4_Taker_Buy_Base_Asset_Volume", "c4_Taker_Buy_Quote_Asset_Volume" ]
 
-X_VARIABLES = C0_VARIABLES + C1_VARIABLES + C2_VARIABLES + C3_VARIABLES + C4_VARIABLES
+X_NUM_VARIABLES = C0_NUM_VARIABLES + C1_NUM_VARIABLES + C2_NUM_VARIABLES + C3_NUM_VARIABLES + C4_NUM_VARIABLES
+X_CAT_VARIABLES = ["candle_pattern0", "candle_pattern1", "candle_pattern2", "candle_pattern3", "candle_pattern4"]
+Y_VARIABLE = ["profit_flag"]
+DATA_PARTITION = 0.20
 
-def getData():
+MAX_ITER = 2000
+HIDDEN_LAYER = (20, 20, 20)
+
+
+def getNumericData():
   #data = session.query(Order).filter(Order.candle0.isnot(None)).all()
   c0 = aliased(Candle)
   c1 = aliased(Candle)
@@ -31,72 +56,151 @@ def getData():
 
   return data
 
-def arrangeData(tupleList):
+
+def getCategoricData():
+  data = session.query(Order).filter(Order.candle_pattern0.isnot(None)).all()
+  return data
+
+
+def arrangeNumericData(tupleList):
   dictList = []
   for item in tupleList:
     (order, c0, c1, c2, c3, c4) = item
-    new_dict = {}
-    new_dict["c0_Open"] = c0.Open
-    new_dict["c0_High"] = c0.High
-    new_dict["c0_Low"] = c0.Low
-    new_dict["c0_Close"] = c0.Close
-    new_dict["c0_Volume"] = c0.Volume
-    new_dict["c0_Quote_Asset_Volume"] = c0.Quote_Asset_Volume
-    new_dict["c0_Number_Of_Trades"] = c0.Number_Of_Trades
-    new_dict["c0_Taker_Buy_Base_Asset_Volume"] = c0.Taker_Buy_Base_Asset_Volume
-    new_dict["c0_Taker_Buy_Quote_Asset_Volume"] = c0.Taker_Buy_Quote_Asset_Volume
-
-    new_dict["c1_Open"] = c1.Open
-    new_dict["c1_High"] = c1.High
-    new_dict["c1_Low"] = c1.Low
-    new_dict["c1_Close"] = c1.Close
-    new_dict["c1_Volume"] = c1.Volume
-    new_dict["c1_Quote_Asset_Volume"] = c1.Quote_Asset_Volume
-    new_dict["c1_Number_Of_Trades"] = c1.Number_Of_Trades
-    new_dict["c1_Taker_Buy_Base_Asset_Volume"] = c1.Taker_Buy_Base_Asset_Volume
-    new_dict["c1_Taker_Buy_Quote_Asset_Volume"] = c1.Taker_Buy_Quote_Asset_Volume
-
-    new_dict["c2_Open"] = c2.Open
-    new_dict["c2_High"] = c2.High
-    new_dict["c2_Low"] = c2.Low
-    new_dict["c2_Close"] = c2.Close
-    new_dict["c2_Volume"] = c2.Volume
-    new_dict["c2_Quote_Asset_Volume"] = c2.Quote_Asset_Volume
-    new_dict["c2_Number_Of_Trades"] = c2.Number_Of_Trades
-    new_dict["c2_Taker_Buy_Base_Asset_Volume"] = c2.Taker_Buy_Base_Asset_Volume
-    new_dict["c2_Taker_Buy_Quote_Asset_Volume"] = c2.Taker_Buy_Quote_Asset_Volume 
-
-    new_dict["c3_Open"] = c3.Open
-    new_dict["c3_High"] = c3.High
-    new_dict["c3_Low"] = c3.Low
-    new_dict["c3_Close"] = c3.Close
-    new_dict["c3_Volume"] = c3.Volume
-    new_dict["c3_Quote_Asset_Volume"] = c3.Quote_Asset_Volume
-    new_dict["c3_Number_Of_Trades"] = c3.Number_Of_Trades
-    new_dict["c3_Taker_Buy_Base_Asset_Volume"] = c3.Taker_Buy_Base_Asset_Volume
-    new_dict["c3_Taker_Buy_Quote_Asset_Volume"] = c3.Taker_Buy_Quote_Asset_Volume   
-
-    new_dict["c4_Open"] = c4.Open
-    new_dict["c4_High"] = c4.High
-    new_dict["c4_Low"] = c4.Low
-    new_dict["c4_Close"] = c4.Close
-    new_dict["c4_Volume"] = c4.Volume
-    new_dict["c4_Quote_Asset_Volume"] = c4.Quote_Asset_Volume
-    new_dict["c4_Number_Of_Trades"] = c4.Number_Of_Trades
-    new_dict["c4_Taker_Buy_Base_Asset_Volume"] = c4.Taker_Buy_Base_Asset_Volume
-    new_dict["c4_Taker_Buy_Quote_Asset_Volume"] = c4.Taker_Buy_Quote_Asset_Volume 
-
-    marker_sell_price = order.marker_sell_price
-    price = order.price
-    profit = (marker_sell_price - price)/price
-    new_dict["profit"] = profit
-    profit_flag = 1 if profit > 0 else 0
-    new_dict["profit_flag"] = profit_flag
-
+    new_dict = createNumericCandleDict(order, c0, c1, c2, c3, c4)
     dictList.append(new_dict)
+
   return dictList
 
-def initML():
-  data = getData()
 
-  data = arrangeData(data)
+def arrangeCategoricalData(itemList):
+  dictList = []
+  for item in itemList:
+    new_dict = createCategoricCandleList(item)
+    dictList.append(new_dict)
+  
+  return dictList
+
+
+def dataProcessingForNumericClassifier(ml_data):
+  X = ml_data[X_NUM_VARIABLES]
+  y = ml_data[Y_VARIABLE]
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = DATA_PARTITION)
+  
+  scaler = StandardScaler()
+  scaler.fit(X_train)
+
+  X_train = scaler.transform(X_train)
+  X_test = scaler.transform(X_test)
+
+  return [X_train, X_test, y_train, y_test]
+
+
+def dataProcessingForCategoricClassifier(ml_data):
+  X = ml_data[X_CAT_VARIABLES]
+  y = ml_data[Y_VARIABLE]
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = DATA_PARTITION)
+  
+  X_train = MultiColumnLabelEncoder(columns = ['candle_pattern0','candle_pattern1', 'candle_pattern2', 'candle_pattern3', 'candle_pattern4']).fit_transform(X_train)
+  X_test = MultiColumnLabelEncoder(columns = ['candle_pattern0','candle_pattern1', 'candle_pattern2', 'candle_pattern3', 'candle_pattern4']).fit_transform(X_test)
+  #le.transform(X_test)
+  #scaler = StandardScaler()
+  #scaler.fit(X_train)
+
+  #X_train = scaler.transform(X_train)
+  #X_test = scaler.transform(X_test)
+
+  return [X_train, X_test, y_train, y_test]
+
+
+def dataProcessingForDT(ml_data):
+  X = ml_data[X_CAT_VARIABLES]
+  y = ml_data[Y_VARIABLE]
+
+  data = ml_data.values
+  X = data[:, :-1].astype(str)
+  y = data[:, -1].astype(str)
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+
+  # one-hot encode input variables
+  onehot_encoder = OneHotEncoder()
+  onehot_encoder.fit(X_train)
+  X_train = onehot_encoder.transform(X_train)
+  #onehot_encoder.fit(X_test)
+  X_test = onehot_encoder.transform(X_test)
+  # ordinal encode target variable
+  label_encoder = LabelEncoder()
+  label_encoder.fit(y_train)
+  y_train = label_encoder.transform(y_train)
+  y_test = label_encoder.transform(y_test)
+
+  return [X_train, X_test, y_train, y_test]
+
+
+def mlpClassifier(X_train, y_train):
+  
+  mlp = MLPClassifier(hidden_layer_sizes=HIDDEN_LAYER, max_iter=MAX_ITER)
+  mlp.fit(X_train, y_train.values.ravel())
+
+  return mlp
+
+
+def DT(X_train, y_train):
+  # Create Decision Tree classifer object
+  clf = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+  # Train Decision Tree Classifer
+  clf = clf.fit(X_train,y_train)
+  return clf
+
+
+def initNumericML():
+  db_data = getNumericData()
+
+  formatted_data = arrangeNumericData(db_data)
+
+  ml_data = pd.DataFrame(formatted_data)
+
+  [X_train, X_test, y_train, y_test] = dataProcessingForNumericClassifier(ml_data)
+
+  model = mlpClassifier(X_train, y_train)
+
+  predictions = model.predict(X_test)
+
+  print(confusion_matrix(y_test,predictions))
+  print(classification_report(y_test,predictions))
+
+
+def initCategoricML():
+  db_data = getCategoricData()
+
+  formatted_data = arrangeCategoricalData(db_data)
+
+  ml_data = pd.DataFrame(formatted_data)
+
+  [X_train, X_test, y_train, y_test] = dataProcessingForCategoricClassifier(ml_data)
+
+  model = mlpClassifier(X_train, y_train)
+
+  predictions = model.predict(X_test)
+
+  print(confusion_matrix(y_test,predictions))
+  print(classification_report(y_test,predictions))
+
+
+def initDTML():
+  db_data = getCategoricData()
+
+  formatted_data = arrangeCategoricalData(db_data)
+
+  ml_data = pd.DataFrame(formatted_data)
+
+  [X_train, X_test, y_train, y_test] = dataProcessingForDT(ml_data)
+
+  clf = DT(X_train, y_train)
+
+    #Predict the response for test dataset
+  y_pred = clf.predict(X_test)
+
+  print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
