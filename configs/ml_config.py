c7e6c38375b01cb7f88b127030bd75305c8cc001
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2020-09-29 01:28:52 +0900

    numeric value to profit probablity in config

--- configs/ml_config.py
+++ configs/ml_config.py
@@ -4,5 +4,5 @@ ml_config = {
   "enable_ml_trade": True,
   "model_file": "mlp_model.txt",
   "scale_file": "mlp_scale.txt",
-  "min_profitable_probablity": "0.9999",
+  "min_profitable_probablity": 0.999,
 }
