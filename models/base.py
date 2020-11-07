a226b725c20df15a10c79712cfa0e8826e836ac8
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2020-11-07 14:00:56 +0900

    major refactoring

--- models/base.py
+++ models/base.py
@@ -1,17 +1,17 @@
 # coding=utf-8
+from configs.config import config
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
-from config import config
 
 url = config.get("db").get("db_url")
 db_type = config.get("db").get("db_type")
 username = config.get("db").get("db_username")
 password = config.get("db").get("db_password")
 full_url = db_type+'://'+username+':'+password + '@'+ url
-
+db_file = 'data.db'
 if config.get("db").get("db_type") == "sqlite":
     full_url = db_type+':///'+db_file
 
