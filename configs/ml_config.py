68fa4df656536cd7e8724bf387271b44e3346888
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2021-01-04 19:26:44 +0900

    fix merge conflict

--- configs/ml_config.py
+++ configs/ml_config.py
@@ -1,11 +1,7 @@
 import os
 
 ml_config = {
-<<<<<<< HEAD
-  "enable_ml_trade": True,
-=======
   "enable_ml_trade": False,
->>>>>>> ee9027b156e99a0d565f06d7b80d2f32cc24f064
   "model_file": "ml/ml_models/mlp_model.txt",
   "scale_file": "ml/ml_models/mlp_scale.txt",
   "min_profitable_probablity": 0.999,
