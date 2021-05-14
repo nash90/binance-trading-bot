47569436e7a438430a38fe43b0f068bb9b3c6496
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2021-05-14 18:42:44 +0900

    round asset change

--- script.py
+++ script.py
@@ -125,7 +125,7 @@ def roundAssetPrice(amount=0, symbol=''):
     elif symbol == 'DOGEBTC':
         return round(amount, 8)
 
-    return round(amount, 6)
+    return round(amount, 8)
 
 def getMyAsset(assetName="BTC"):
     asset = client.get_asset_balance(asset=assetName)
